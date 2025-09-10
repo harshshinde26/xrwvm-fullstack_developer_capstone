1cd315b2b5f5eb0043e314d76c119ac83dc3f141
harshshinde26 <harshshinde818@gmail.com>
2025-09-10 15:39:51 -0400

    auth

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import register_container from"./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<register_container />} />
     </Routes>
   );
 }
